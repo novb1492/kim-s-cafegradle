bcf7daf432b46c4fa3aebcffb7b1945c3881f1fe
novb1492 <novb1492@naver.com>
2021-06-10 10:59:43 +0900

    commentdto조건걸음

--- src/main/java/com/example/kim_s_cafe/model/comment/commentdto.java
+++ src/main/java/com/example/kim_s_cafe/model/comment/commentdto.java
@@ -2,18 +2,25 @@ package com.example.kim_s_cafe.model.comment;
 
 import java.sql.Timestamp;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class commentdto {
     
-
+    @NotNull
     private int cid;
-    
+
+    @NotNull
     private int bid;
 
+    @Email
+    @NotNull
     private String email;
-
+    
+    @NotNull
     private String comment;
 
     private Timestamp created;
