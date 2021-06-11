12d866fda93f6790d162c11c5dcbd7631e14694b
novb1492 <novb1492@naver.com>
2021-06-11 20:59:42 +0900

    j쿼리를 바닐라 js로 바꾸는법 그냥 .id하면된다

--- src/main/java/com/example/kim_s_cafe/config/security.java
+++ src/main/java/com/example/kim_s_cafe/config/security.java
@@ -5,12 +5,13 @@ package com.example.kim_s_cafe.config;
 
 
 
+
+
 import com.example.kim_s_cafe.config.oauth.principaloauth2userservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -53,10 +54,10 @@ public class security extends WebSecurityConfigurerAdapter {
        http
          .csrf().disable()///ajax사용하기 위해 토큰은 나중에
          .authorizeRequests()////요청이발생
-         .antMatchers("/","/auth/**","/oauth2/")////이 링크들은
+         .antMatchers("/","/auth/**","/static/**")////이 링크들은
          .permitAll()///허용한다
-         .anyRequest()///그외 다른 요청운
-         .authenticated()//인증이있어야한다(로그인)
+         //.anyRequest()///그외 다른 요청운
+         //.authenticated()//인증이있어야한다(로그인)
         .and()
             .formLogin()////로그인 발생시
             .loginPage("/auth/loginpage")///로그인페이지 지정
@@ -66,13 +67,8 @@ public class security extends WebSecurityConfigurerAdapter {
             .oauth2Login()
             .loginPage("/auth/loginpage")
             .userInfoEndpoint()
-            .userService(principaloauth2userservice);
-            
-
-         
- 
-           
-        
-            
+            .userService(principaloauth2userservice);          
     }
+
+
 }
---
+++ src/main/resources/static/ajaxlib.js
@@ -0,0 +1,17 @@
+window.onload=function(){
+
+    document.querySelectorAll('.button').forEach(function(item) {
+    item.addEventListener('click', function() {
+            var xhr = new XMLHttpRequest(); //new로 생성
+            xhr.open('POST', '/auth/test', true); //j쿼리 $ajax.({type,url},true가 비동기)
+            xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");////이게없으면 psot전송불가 조금 찾았네
+            xhr.send("test="+item.id); /// ajax data부분
+            xhr.onload = function() { 
+                if(xhr.status==200){ // success:function(data)부분 통신 성공시 200반환
+                    alert(xhr.response);
+                }
+            }
+    });
+    });
+     
+}
