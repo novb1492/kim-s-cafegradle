4523e344b6875b84205ba4745801958a99c829f3
novb1492 <novb1492@naver.com>
2021-06-11 21:41:24 +0900

    태그에 묻혀주기

--- src/main/java/com/example/kim_s_cafe/contoroller/restcontroller.java
+++ src/main/java/com/example/kim_s_cafe/contoroller/restcontroller.java
@@ -47,9 +47,10 @@ public class restcontroller {
     private utilservice utilservice;
     
     @PostMapping("/auth/test")
-    public String test(@RequestParam("test")String test){
+    public String test(@RequestParam("test")String test,@RequestParam("test2")String test2){
 
         System.out.println("test"+ test);
+        System.out.println("test2"+ test2);
         return "false";
     }
     @PostMapping("/auth/comfirm")
--- src/main/resources/static/ajaxlib.js
+++ src/main/resources/static/ajaxlib.js
@@ -1,17 +1,19 @@
-window.onload=function(){
 
-    document.querySelectorAll('.button').forEach(function(item) {
-    item.addEventListener('click', function() {
-            var xhr = new XMLHttpRequest(); //new로 생성
-            xhr.open('POST', '/auth/test', true); //j쿼리 $ajax.({type,url},true가 비동기)
-            xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");////이게없으면 psot전송불가 조금 찾았네
-            xhr.send("test="+item.id); /// ajax data부분
-            xhr.onload = function() { 
-                if(xhr.status==200){ // success:function(data)부분 통신 성공시 200반환
-                    alert(xhr.response);
-                }
-            }
-    });
-    });
-     
+window.onload=function(){
+ 
+        document.querySelectorAll('.button').forEach(function(item) {
+            item.addEventListener('click', function() {
+                    var xhr = new XMLHttpRequest(); //new로 생성
+                    xhr.open('POST', item.id, true); //j쿼리 $ajax.({type,url},true가 비동기)
+                    xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");////이게없으면 psot전송불가 조금 찾았네
+                    xhr.send("test="+item.name); /// ajax data부분
+                    xhr.onload = function() { 
+                        if(xhr.status==200){ // success:function(data)부분 통신 성공시 200반환
+                            alert(xhr.response);
+                        }
+                    }
+            });
+            });  
+    
+    
 }
