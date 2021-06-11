82b85e8af07e76982272ac648148a0671575ca51
novb1492 <novb1492@naver.com>
2021-06-12 00:01:42 +0900

    찾았다 다른클래스 값 찾아온는법 이제 j쿼리 싹다 제거가능하다

--- src/main/resources/static/ajaxlib.js
+++ src/main/resources/static/ajaxlib.js
@@ -1,12 +1,15 @@
-
 window.onload=function(){
- 
+
         document.querySelectorAll('.button').forEach(function(item) {
+          
+           
             item.addEventListener('click', function() {
                     var xhr = new XMLHttpRequest(); //new로 생성
+                    var value=document.getElementsByClassName(item.name)[0];
+                    alert(value.name);
                     xhr.open('POST', item.id, true); //j쿼리 $ajax.({type,url},true가 비동기)
                     xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");////이게없으면 psot전송불가 조금 찾았네
-                    xhr.send("test="+item.name); /// ajax data부분
+                    xhr.send("test="+item.name+"comment"); /// ajax data부분
                     xhr.onload = function() { 
                         if(xhr.status==200){ // success:function(data)부분 통신 성공시 200반환
                             alert(xhr.response);
